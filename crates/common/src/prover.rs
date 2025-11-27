1a4b3d64d71453354ab56f1676ef09bc324f7357
leruaa <aurelien.catinon@gmail.com>
2025-11-27 14:08:15 +0100

    feat: add mock feature

--- crates/common/src/prover.rs
+++ crates/common/src/prover.rs
@@ -1,20 +1,24 @@
 use eyre::{Result, eyre};
-use sp1_sdk::{EnvProver, ProverClient, SP1ProofWithPublicValues, SP1ProvingKey, SP1Stdin};
+use sp1_prover::components::CpuProverComponents;
+use sp1_sdk::{ProverClient, SP1ProofMode, SP1ProofWithPublicValues, SP1ProvingKey, SP1Stdin};
 
 pub struct Prover {
-    client: EnvProver,
+    client: Box<dyn sp1_sdk::Prover<CpuProverComponents>>,
     elf: Vec<u8>,
     pk: SP1ProvingKey,
 }
 
 impl Prover {
     pub fn new(elf: &[u8]) -> Self {
+        #[cfg(feature = "mock")]
+        let client = ProverClient::builder().mock().build();
+        #[cfg(not(feature = "mock"))]
         let client = ProverClient::from_env();
 
         let (pk, _) = client.setup(elf);
 
         Self {
-            client,
+            client: Box::new(client),
             elf: elf.to_vec(),
             pk,
         }
@@ -23,16 +27,14 @@ impl Prover {
     pub fn prove<I: Into<SP1Stdin>>(&self, inputs: I) -> Result<SP1ProofWithPublicValues> {
         let stdin = inputs.into();
 
-        let (public_values, report) = self.client.execute(&self.elf, &stdin).run().unwrap();
+        let (public_values, report) = self.client.execute(&self.elf, &stdin).unwrap();
 
         println!("Cycles: {}", report.total_instruction_count());
         println!("Public values: {}", hex::encode(public_values.to_vec()));
 
         let proof = self
             .client
-            .prove(&self.pk, &stdin)
-            .groth16()
-            .run()
+            .prove(&self.pk, &stdin, SP1ProofMode::Groth16)
             .map_err(|err| eyre!("{err}"))?;
 
         Ok(proof)
