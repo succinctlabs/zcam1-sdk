192d427ea7338fe9c3514534822d7899be7df1f1
leruaa <aurelien.catinon@gmail.com>
2025-12-03 17:34:07 +0100

    feat: init prove example

---
+++ examples/prove/app/index.tsx
@@ -0,0 +1,87 @@
+import { StyleSheet, Button, View } from "react-native";
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { launchImageLibrary } from "react-native-image-picker";
+import { DeviceInfo, embedProof, initDevice } from "react-native-zcam1-prove";
+import { useEffect, useMemo, useState } from "react";
+
+export default function Index() {
+  const appId = process.env.EXPO_PUBLIC_APP_ID!;
+  const [deviceInfo, setDeviceInfo] = useState<DeviceInfo | undefined>(
+    undefined,
+  );
+
+  const settings = useMemo(() => {
+    return {
+      appId,
+      backendUrl: process.env.EXPO_PUBLIC_BACKEND_URL!,
+      production: false,
+    };
+  }, [appId]);
+
+  useEffect(() => {
+    async function fetchDevice() {
+      const deviceInfo = await initDevice(settings);
+      setDeviceInfo(deviceInfo);
+    }
+
+    fetchDevice();
+  }, [settings]);
+
+  const pick = async () => {
+    const result = await launchImageLibrary({
+      mediaType: "photo",
+      selectionLimit: 1,
+    });
+
+    console.log("File", result.assets![0].uri);
+
+    if (deviceInfo !== undefined) {
+      const outputPath = await embedProof(
+        result.assets![0].uri!,
+        deviceInfo,
+        settings,
+      );
+
+      console.log("Output File", outputPath);
+
+      try {
+        let res = await CameraRoll.saveAsset(outputPath);
+        console.log("Saved: " + res.node.image.filename);
+      } catch (error) {
+        console.error("Error saving photo:", error);
+      }
+    }
+  };
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={{ flex: 1 }}>
+          <Button title="Pick photo" onPress={pick} />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
--- justfile
+++ justfile
@@ -12,6 +12,14 @@ update-react-native-zcam1-capture $NPM_CONFIG_REGISTRY: (update-react-native-zca
     npm unpublish --force
     npm publish
 
+update-react-native-zcam1-prove $NPM_CONFIG_REGISTRY: (update-react-native-zcam1-c2pa NPM_CONFIG_REGISTRY)
+    #!/usr/bin/env sh
+    cd react-native-zcam1-prove
+    npm rm react-native-zcam1-c2pa
+    npm i react-native-zcam1-c2pa@latest --force
+    npm unpublish --force
+    npm publish
+
 update-react-native-zcam1-verify $NPM_CONFIG_REGISTRY: (update-react-native-zcam1-c2pa NPM_CONFIG_REGISTRY)
     #!/usr/bin/env sh
     cd react-native-zcam1-verify
@@ -26,6 +34,12 @@ update-capture-example $NPM_CONFIG_REGISTRY: (update-react-native-zcam1-capture 
     npm rm react-native-zcam1-capture
     npm i react-native-zcam1-capture@latest --force
 
+update-prove-example $NPM_CONFIG_REGISTRY: (update-react-native-zcam1-prove NPM_CONFIG_REGISTRY)
+    #!/usr/bin/env sh
+    cd examples/prove
+    npm rm react-native-zcam1-prove
+    npm i react-native-zcam1-prove@latest --force
+
 update-verify-example $NPM_CONFIG_REGISTRY: (update-react-native-zcam1-verify NPM_CONFIG_REGISTRY)
     #!/usr/bin/env sh
     cd examples/verify
@@ -35,6 +49,11 @@ update-verify-example $NPM_CONFIG_REGISTRY: (update-react-native-zcam1-verify NP
 run-capture-example DEVICE:
     #!/usr/bin/env sh
     cd examples/capture
+    npx expo prebuild && npx expo run:ios --device {{DEVICE}}
+
+run-prove-example DEVICE:
+    #!/usr/bin/env sh
+    cd examples/prove
     npx expo prebuild && npx expo run:ios --device {{DEVICE}}
 
 run-verify-example DEVICE:
