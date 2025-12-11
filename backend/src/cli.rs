e4dfe3896df427dc5e2240f4f54de58eeee8298d
leruaa <aurelien.catinon@gmail.com>
2025-12-11 10:19:24 +0100

    feat: add tracing

--- backend/src/cli.rs
+++ backend/src/cli.rs
@@ -5,4 +5,7 @@ use clap::Parser;
 pub struct Args {
     #[clap(long, env, default_value_t = 8081)]
     pub port: u16,
+
+    #[clap(long, env)]
+    pub logs_path: Option<String>,
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,9 @@
 use clap::Parser;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_appender::rolling;
+use tracing_subscriber::{
+    EnvFilter, Layer, fmt::layer, layer::SubscriberExt, util::SubscriberInitExt,
+};
 use zcam1_server::build_app;
 
 use crate::cli::Args;
@@ -13,7 +18,37 @@ async fn main() {
 
     let app = build_app();
 
+    let (file_layer, _guard) = if let Some(logs_path) = args.logs_path {
+        // Create a rolling file appender
+        let file_appender = rolling::never(logs_path, "logs.txt");
+
+        // Create a layer that writes to the file
+        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+        let layer = layer()
+            .compact()
+            .with_target(false)
+            .with_writer(non_blocking)
+            .with_filter(LevelFilter::INFO);
+
+        (Some(layer), Some(_guard))
+    } else {
+        (None, None)
+    };
+
+    tracing_subscriber::registry()
+        .with(
+            layer().compact().with_target(false).with_filter(
+                EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .from_env_lossy(),
+            ),
+        )
+        .with(file_layer)
+        .init();
+
     // run our app with hyper, listening globally on port 3000
+    info!("Starting server on port {}", args.port);
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", args.port))
         .await
         .unwrap();
