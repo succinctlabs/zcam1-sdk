e07b1c04410c6d03c3cdac30fdb1f0348a57599b
leruaa <aurelien.catinon@gmail.com>
2025-12-27 14:12:47 +0100

    feat: better handle prover client initialization

--- crates/proving-utils/src/error.rs
+++ crates/proving-utils/src/error.rs
@@ -3,6 +3,9 @@ use thiserror::Error;
 #[derive(Debug, Error, uniffi::Error)]
 #[uniffi(flat_error)]
 pub enum Error {
+    #[error("Prover not initialized")]
+    ProverNotInitialized,
+
     #[error("{0}")]
     Sp1(String),
 }
--- crates/proving-utils/src/prover.rs
+++ crates/proving-utils/src/prover.rs
@@ -1,4 +1,8 @@
-use std::marker::PhantomData;
+use std::{
+    marker::PhantomData,
+    sync::{Arc, OnceLock},
+    thread,
+};
 
 use serde::{Deserialize, Serialize};
 use sp1_sdk::{
@@ -12,19 +16,24 @@ use crate::error::Error;
 pub const IOS_AUTHENCITY_ELF: &[u8] = include_elf!("authenticity-ios");
 pub const MOCK_ELF: &[u8] = include_elf!("mock");
 
+#[uniffi::export(callback_interface)]
+pub trait Initialized: Send + Sync {
+    fn initialized(&self);
+}
+
 #[derive(uniffi::Object)]
 pub struct IosProvingClient(ProvingClient<IosAuthInputs>);
 
 #[uniffi::export]
 impl IosProvingClient {
-    #[uniffi::constructor()]
-    pub fn new(private_key: String) -> Self {
-        Self(ProvingClient::ios(private_key))
+    #[uniffi::constructor(default(callback = None))]
+    pub fn new(private_key: String, callback: Option<Box<dyn Initialized>>) -> Self {
+        Self(ProvingClient::ios(private_key, callback))
     }
 
-    #[uniffi::constructor()]
-    pub fn mock() -> Self {
-        Self(ProvingClient::mock())
+    #[uniffi::constructor(default(callback = None))]
+    pub fn mock(callback: Option<Box<dyn Initialized>>) -> Self {
+        Self(ProvingClient::mock(callback))
     }
 
     pub async fn request_proof(&self, inputs: IosAuthInputs) -> Result<Vec<u8>, Error> {
@@ -34,15 +43,15 @@ impl IosProvingClient {
             .map(|proof| proof.bytes())
     }
 
-    pub fn vk_hash(&self) -> String {
+    pub fn vk_hash(&self) -> Result<String, Error> {
         self.0.vk_hash()
     }
 }
 
 pub struct ProvingClient<I> {
-    prover: EitherProver,
-    pk: SP1ProvingKey,
-    vk_hash: String,
+    prover: Arc<OnceLock<EitherProver>>,
+    pk: Arc<OnceLock<SP1ProvingKey>>,
+    vk_hash: Arc<OnceLock<String>>,
     phantom: PhantomData<I>,
 }
 
@@ -50,44 +59,80 @@ impl<I> ProvingClient<I>
 where
     I: Into<SP1Stdin>,
 {
-    pub fn mock() -> Self {
-        let prover = CpuProver::mock();
+    pub fn mock(callback: Option<Box<dyn Initialized>>) -> Self {
+        let prover = Arc::new(OnceLock::new());
+        let pk = Arc::new(OnceLock::new());
+        let vk_hash = Arc::new(OnceLock::new());
+        let cloned_prover = prover.clone();
+        let cloned_pk = pk.clone();
+        let cloned_vk_hash = vk_hash.clone();
 
-        let (pk, vk) = prover.setup(MOCK_ELF);
+        thread::spawn(move || {
+            let prover = CpuProver::mock();
+            let (pk, vk) = prover.setup(MOCK_ELF);
+            let _ = cloned_prover.set(EitherProver::Mock {
+                prover: Box::new(prover),
+            });
+            let _ = cloned_pk.set(pk);
+            let _ = cloned_vk_hash.set(vk.bytes32());
+
+            if let Some(callback) = callback {
+                callback.initialized();
+            }
+        });
 
         Self {
-            prover: EitherProver::Mock {
-                prover: Box::new(prover),
-            },
+            prover,
             pk,
-            vk_hash: vk.bytes32(),
+            vk_hash,
             phantom: PhantomData,
         }
     }
 
     pub async fn request_proof(&self, inputs: I) -> Result<SP1ProofWithPublicValues, Error> {
-        self.prover.request_proof(&self.pk, inputs.into()).await
+        let pk = self.pk.get().ok_or(Error::ProverNotInitialized)?;
+        let prover = self.prover.get().ok_or(Error::ProverNotInitialized)?;
+        prover.request_proof(pk, inputs.into()).await
     }
 
-    pub fn vk_hash(&self) -> String {
-        self.vk_hash.clone()
+    pub fn vk_hash(&self) -> Result<String, Error> {
+        self.vk_hash
+            .get()
+            .ok_or(Error::ProverNotInitialized)
+            .cloned()
     }
 }
 
 impl ProvingClient<IosAuthInputs> {
-    pub fn ios(private_key: String) -> Self {
+    pub fn ios(private_key: String, callback: Option<Box<dyn Initialized>>) -> Self {
         let rpc_url = get_default_rpc_url_for_mode(NetworkMode::Reserved);
         let signer = NetworkSigner::local(&private_key).unwrap();
-        let prover = NetworkProver::new(signer, &rpc_url, NetworkMode::Reserved);
 
-        let (pk, vk) = prover.setup(IOS_AUTHENCITY_ELF);
+        let prover = Arc::new(OnceLock::new());
+        let pk = Arc::new(OnceLock::new());
+        let vk_hash = Arc::new(OnceLock::new());
+        let cloned_prover = prover.clone();
+        let cloned_pk = pk.clone();
+        let cloned_vk_hash = vk_hash.clone();
+
+        thread::spawn(move || {
+            let prover = NetworkProver::new(signer, &rpc_url, NetworkMode::Reserved);
+            let (pk, vk) = prover.setup(IOS_AUTHENCITY_ELF);
+            let _ = cloned_prover.set(EitherProver::Network {
+                prover: Box::new(prover),
+            });
+            let _ = cloned_pk.set(pk);
+            let _ = cloned_vk_hash.set(vk.bytes32());
+
+            if let Some(callback) = callback {
+                callback.initialized();
+            }
+        });
 
         Self {
-            prover: EitherProver::Network {
-                prover: Box::new(prover),
-            },
+            prover,
             pk,
-            vk_hash: vk.bytes32(),
+            vk_hash,
             phantom: PhantomData,
         }
     }
