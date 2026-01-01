57a1b637dd6d427d85703a02db5604c49088587c
leruaa <aurelien.catinon@gmail.com>
2026-01-01 16:25:29 +0100

    feat: allow to skip framework download

--- react-native-zcam1-c2pa/scripts/download-framework.sh
+++ react-native-zcam1-c2pa/scripts/download-framework.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# 1. Get the version from package.json
+#  Get the version from package.json
 VERSION=$(node -p "require('./package.json').version")
 
-# 2. Define constants
+# Define constants
 URL="https://github.com/succinctlabs/zcam1-sdk/releases/download/v$VERSION/Zcam1C2paFramework.xcframework.zip"
 ZIP_FILE="Zcam1C2paFramework.xcframework.zip"
 FRAMEWORK_DIR="Zcam1C2paFramework.xcframework"
 
-# 3. Check if framework already exists
+# Check if framework already exists
 if [ -d "$FRAMEWORK_DIR" ]; then
   echo "✅ $FRAMEWORK_DIR already exists. Skipping download."
   exit 0
 fi
 
+# Skip if environment variable is set
+if [ -n "$SKIP_FRAMEWORK_DOWNLOAD" ]; then
+  echo "⏭️  SKIP_FRAMEWORK_DOWNLOAD is set. Skipping framework download."
+  exit 0
+fi
+
+
 echo "📦 Preparing to download version v$VERSION..."
 
-# 4. Download the file
+# Download the file
 # -L follows redirects (crucial for GitHub)
 # -f fails silently on server errors (404, etc) so we can catch them
 if curl -L -f -o "$ZIP_FILE" "$URL"; then
@@ -26,12 +33,12 @@ else
   exit 1
 fi
 
-# 5. Extract the file
+# Extract the file
 echo "📂 Extracting..."
 # -o overwrites existing files without asking
 # -q is quiet mode
 unzip -o -q "$ZIP_FILE"
 
-# 6. Cleanup
+# Cleanup
 rm "$ZIP_FILE"
 echo "✨ Done! $FRAMEWORK_DIR is ready."
