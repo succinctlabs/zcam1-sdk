10ba7d199fd61e43e382d92c2f5bcb7d5cb0e9b8
leruaa <aurelien.catinon@gmail.com>
2026-01-13 11:01:35 +0100

    fix: rollback react-native-vision-camera in e2e example

--- examples/e2e/app/index.tsx
+++ examples/e2e/app/index.tsx
@@ -3,27 +3,19 @@ import { StyleSheet, Button, View, Text } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FileSystem, Util } from "react-native-file-access";
 import {
-  buildSelfSignedCertificate,
   CaptureInfo,
-  embedBindings,
   initCapture,
+  ZCamera,
 } from "@succinctlabs/react-native-zcam1-capture";
-import {
-  Camera,
-  useCameraDevice,
-  useCameraPermission,
-} from "react-native-vision-camera";
 import Toast from "react-native-toast-message";
 import { privateDirectory } from "@succinctlabs/react-native-zcam1-picker";
 
 export default function Home() {
-  const device = useCameraDevice("back");
-  const { requestPermission } = useCameraPermission();
-  const camera = useRef<Camera>(null);
   const appId = process.env.EXPO_PUBLIC_APP_ID!;
   const [captureInfo, setCaptureInfo] = useState<CaptureInfo | undefined>(
     undefined,
   );
+  const camera = useRef<ZCamera>(null);
 
   const settings = useMemo(() => {
     return {
@@ -41,60 +33,30 @@ export default function Home() {
     fetchDevice();
   }, [settings]);
 
-  useEffect(() => {
-    requestPermission();
-  }, [requestPermission]);
-
-  const certChainPem = useMemo(() => {
-    if (captureInfo) {
-      return buildSelfSignedCertificate(captureInfo.contentPublicKey);
-    }
-  }, [captureInfo]);
-
   const capture = async () => {
-    if (camera.current && captureInfo && certChainPem) {
-      const photo = await camera.current.takePhoto();
-      const targetPath = privateDirectory();
-      const targetFile = targetPath + "/" + Util.basename(photo.path!);
+    const photo = await camera.current?.takePhoto();
+    const targetPath = privateDirectory();
+    const targetFile = targetPath + "/" + Util.basename(photo?.path!);
 
-      await FileSystem.exists(targetPath).then((exists) => {
-        if (!exists) return FileSystem.mkdir(targetPath);
-      });
+    await FileSystem.exists(targetPath).then((exists) => {
+      if (!exists) return FileSystem.mkdir(targetPath);
+    });
 
-      let photoWithBindings = await embedBindings(
-        photo.path,
-        "",
-        {
-          device_make: "",
-          device_model: "",
-          software_version: "",
-        },
-        captureInfo,
-        certChainPem,
-      );
+    await FileSystem.cp(photo?.path!, targetFile);
 
-      await FileSystem.cp(photoWithBindings, targetFile);
-
-      Toast.show({
-        type: "success",
-        text1: "A photo has been captured",
-        text2: "And saved to an internal folder",
-      });
-    }
+    Toast.show({
+      type: "success",
+      text1: "A photo has been captured",
+      text2: "And saved to an internal folder",
+    });
   };
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View style={{ flex: 1 }}>
-          {device ? (
-            <Camera
-              style={StyleSheet.absoluteFill}
-              ref={camera}
-              device={device}
-              isActive={true}
-              photo={true}
-            />
+          {captureInfo ? (
+            <ZCamera ref={camera} captureInfo={captureInfo} />
           ) : (
             <View
               style={{
@@ -103,16 +65,12 @@ export default function Home() {
                 alignItems: "center",
               }}
             >
-              <Text>No camera device found</Text>
+              <Text>Initializing device...</Text>
             </View>
           )}
         </View>
         <View>
-          <Button
-            title="Capture"
-            onPress={capture}
-            disabled={!camera.current || !captureInfo || !certChainPem}
-          />
+          <Button title="Capture" onPress={capture} />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
