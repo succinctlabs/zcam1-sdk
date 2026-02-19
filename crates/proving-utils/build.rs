a5eb33ea352874c657de6ed7b28d45017f9d4290
leruaa <aurelien.catinon@gmail.com>
2026-02-19 13:20:11 +0100

    feat: migrate to Hypercube

--- crates/proving-utils/build.rs
+++ crates/proving-utils/build.rs
@@ -1,14 +1,37 @@
 use std::fs::{self, File};
 
-use sp1_build::{build_program, verifying_key};
+use sp1_build::{build_program, generate_elf_paths};
+use sp1_prover::worker::SP1LightNode;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     build_program("../../programs/authenticity-ios");
     build_program("../../programs/mock");
 
-    fs::create_dir_all("./artifacts").unwrap();
-    let vk = verifying_key("../../programs/authenticity-ios", "authenticity-ios");
-    let vk_file = File::create("./artifacts/vk.bin").unwrap();
+    let program_dir = std::path::Path::new("../../programs/authenticity-ios");
+    let metadata_file = program_dir.join("Cargo.toml");
+    let mut metadata_cmd = cargo_metadata::MetadataCommand::new();
+    let metadata = metadata_cmd.manifest_path(metadata_file).exec()?;
+    let elf_paths = generate_elf_paths(&metadata, None)?;
 
-    bincode::serialize_into(vk_file, &vk).unwrap();
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+
+    rt.block_on(async {
+        fs::create_dir_all("./artifacts")?;
+
+        let prover = SP1LightNode::new().await;
+
+        for (_, elf_path) in elf_paths {
+            let elf = fs::read(&elf_path)?;
+
+            let vk = prover.setup(&elf).await?;
+
+            let vk_file = File::create("./artifacts/vk.bin")?;
+
+            bincode::serialize_into(vk_file, &vk)?;
+        }
+
+        Ok(())
+    })
 }
