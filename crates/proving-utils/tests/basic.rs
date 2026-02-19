f180c01fd705021d9eece46e1ef9951090a3200a
leruaa <aurelien.catinon@gmail.com>
2026-02-19 13:20:43 +0100

    feat: update test

--- crates/proving-utils/tests/basic.rs
+++ crates/proving-utils/tests/basic.rs
@@ -1,19 +1,22 @@
-use sp1_sdk::EnvProver;
+use sp1_sdk::{
+    NetworkProver, ProveRequest, Prover, SP1Stdin,
+    env::EnvProver,
+    network::{NetworkMode, get_default_rpc_url_for_mode, signer::NetworkSigner},
+};
 use zcam1_ios::AuthInputs;
 use zcam1_proving_utils::IOS_AUTHENCITY_ELF;
 
-#[test]
-fn execute_in_sp1_test() {
+#[tokio::test]
+async fn execute_in_sp1_test() {
     let inputs = AuthInputs {
-        photo_bytes: std::fs::read("./tests/fixtures/with_bindings.jpg").unwrap(),
+        photo_bytes: std::fs::read("./tests/fixtures/with_bindings.jpg")
+            .expect("Failed to read with_bindings.jpg"),
         format: "image/jpeg".to_string(),
         app_attest_production: false,
     };
 
-    let prover = EnvProver::new();
-
     prover
-        .execute(IOS_AUTHENCITY_ELF, &inputs.into())
-        .run()
+        .execute(IOS_AUTHENCITY_ELF, inputs.into())
+        .await
         .unwrap();
 }
