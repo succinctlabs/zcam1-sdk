c585c5eaa001a9637c9eeaebe3954eec63da38be
leruaa <aurelien.catinon@gmail.com>
2026-03-03 11:49:27 +0100

    feat: add verify_proof_from_manifest()

--- crates/verify-utils/src/proofs.rs
+++ crates/verify-utils/src/proofs.rs
@@ -1,6 +1,22 @@
 use sp1_verifier::{GROTH16_VK_BYTES, Groth16Verifier};
+use zcam1_ios::APPLE_ROOT_CERT;
 
 use crate::error::VerifyError;
+
+pub fn verify_proof_from_manifest(
+    proof: &[u8],
+    vk_hash: &str,
+    photo_hash: &[u8],
+    app_id: &str,
+) -> Result<bool, VerifyError> {
+    let mut public_inputs = vec![];
+
+    public_inputs.extend_from_slice(photo_hash);
+    public_inputs.extend_from_slice(app_id.as_bytes());
+    public_inputs.extend_from_slice(APPLE_ROOT_CERT.as_bytes());
+
+    verify_groth16(proof, &public_inputs, vk_hash)
+}
 
 /// Wrapper around [`sp1_verifier::Groth16Verifier::verify`].
 ///
