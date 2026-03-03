da681cb81b23aa2c0eea143db658af27e910d3d6
leruaa <aurelien.catinon@gmail.com>
2026-03-03 11:50:32 +0100

    chore: more cleanup

--- crates/verify-bindings/src/error.rs
+++
@@ -1,8 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error, uniffi::Error)]
-#[uniffi(flat_error)]
-pub enum VerifyError {
-    #[error(transparent)]
-    AppAttest(#[from] zcam1_ios::Error),
-}
