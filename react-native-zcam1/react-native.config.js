2e96f35799e00cc08794e233a86e417a73dbcc27
leruaa <aurelien.catinon@gmail.com>
2026-03-11 16:52:19 +0100

    feat: finish Android capture impl

--- react-native-zcam1/react-native.config.js
+++ react-native-zcam1/react-native.config.js
@@ -3,8 +3,8 @@ module.exports = {
     platforms: {
       android: {
         packageImportPath:
-          'import com.succinctlabs.zcam1sdk.Zcam1SdkPackage;\nimport com.succinctlabs.zcam1sdk.Zcam1ProvingPackage;',
-        packageInstance: 'new Zcam1SdkPackage(), new Zcam1ProvingPackage()',
+          'import com.succinctlabs.zcam1sdk.Zcam1SdkPackage;\nimport com.succinctlabs.zcam1sdk.Zcam1ProvingPackage;\nimport com.succinctlabs.zcam1sdk.Zcam1CapturePackage;',
+        packageInstance: 'new Zcam1SdkPackage(), new Zcam1ProvingPackage(), new Zcam1CapturePackage()',
       },
     },
   },
