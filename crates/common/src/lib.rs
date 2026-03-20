efd8887dede422cf287a17b9ff85ef422cfd8bf4
leruaa <aurelien.catinon@gmail.com>
2026-03-20 14:40:00 +0100

    fix: merge conflict

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -4,7 +4,4 @@ pub use constants::{APPLE_ROOT_CERT, GOOGLE_HARDWARE_ROOT_EC, GOOGLE_HARDWARE_RO
 mod io;
 pub use io::AuthInputs;
 
-mod verifier;
-pub use verifier::{Verifier, VerifierError};
-
 uniffi::setup_scaffolding!();
